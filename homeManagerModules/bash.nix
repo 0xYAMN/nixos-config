2bcb4f81b8de9516fb29e865d1110a0e3d2d5135
Yannick Monjeamb <yannick.monjeamb@rwth-aachen.de>
2026-07-28 13:43:33 +0200

    Add function to bashrc to change terminal dir via yazi

--- homeManagerModules/bash.nix
+++ homeManagerModules/bash.nix
@@ -19,6 +19,14 @@
 
       initExtra = ''
         
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          command yazi "$@" --cwd-file="$tmp"
+          IFS= read -r -d \'\' cwd < "$tmp"
+          [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+          command rm -f -- "$tmp"
+        }
+
         nix_shell_status() {
             if [ "$IN_NIX_SHELL" = "in-pure-shell" ]; then
                 echo "(pure-nix) "
